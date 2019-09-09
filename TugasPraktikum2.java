fef61a147f856aea3ec11c9274e912a318c2669d
Akmal Zuhdy Prasetya <54744527+trueazp@users.noreply.github.com>
2019-09-09 19:25:07 +0800

    Add TugasPraktikum.java file

---
+++ TugasPraktikum2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class TugasPraktikum2 {
+
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+
+        int detik, menit, jam;
+        
+        System.out.print("Masukkan detik : ");
+        detik = in.nextInt();
+
+        jam = detik/3600; detik = detik - (3600*jam);
+        menit = detik/60; detik = detik - (60*menit);
+
+        System.out.print("Hasil konversi : " + jam + " : " + menit + " : " + detik);
+    }
+}
